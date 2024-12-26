b2e9ccb46986f4474457ca5d3aa611ab38e8a6fd
fulfix <fulfix.sellier@gmail.com>
2024-12-26 20:35:39 +0100

    test

--- jsp.sh
+++ jsp.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
-# Ajouter la variable d'environnement LD_LIBRARY_PATH à ~/.bashrc
-echo "export LD_LIBRARY_PATH=/usr/local/lib64:$LD_LIBRARY_PATH" >> ~/.bashrc
-source ~/.bashrc
+if ! grep -q "export LD_LIBRARY_PATH=/usr/local/lib64" ~/.bashrc; then
+    if [[ -z "${LD_LIBRARY_PATH}" ]]; then
+        echo "export LD_LIBRARY_PATH=/usr/local/lib64" >> ~/.bashrc
+    else
+        echo "export LD_LIBRARY_PATH=/usr/local/lib64:${LD_LIBRARY_PATH}" >> ~/.bashrc
+    fi
+    echo "La variable LD_LIBRARY_PATH a été ajoutée à .bashrc"
+else
+    echo "La variable LD_LIBRARY_PATH existe déjà dans .bashrc"
+fi
